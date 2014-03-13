0187aae7ff0fa85f0a477c3d6bce3299c0aa58a1
Bryce Hill <bryceahill@gmail.com>
2014-03-13 11:06:13 -0700

    Add gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename')
+    rename = require('gulp-rename'),
+    livereload = require('gulp-livereload')
 
 gulp.task('build', function() {
     return gulp.src('./src/Thermometer.js')
         .pipe(uglify())
         .pipe(rename('thermometer.min.js'))
         .pipe(gulp.dest('./dist'));
+})
+
+gulp.task('watch', function() {
+    // var server = livereload()
+    gulp.watch('./src/Thermometer.js', ['build'])
 })
